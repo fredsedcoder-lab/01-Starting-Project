25d7a49c361bd00eca9052b263b5f8382cb4ba19
fred <fredsedcoder@gmail.com>
2026-01-31 10:43:56 +0100

    add cypress task page suite and 2 tests in it

---
+++ cypress/e2e/basics.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe('tasks page', () => {
+  it('Should render the main image', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('.main-header img').should('be.visible')
+    
+  });
+  it('Should display the page title', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('.main-header h1').should('contain.text', 'React Tasks')
+  });
+})
