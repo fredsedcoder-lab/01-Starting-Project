02f1a012f09f51d46bbc13f42db6d50b411e2865
fred <fredsedcoder@gmail.com>
2026-02-01 22:36:06 +0100

    add cypress basics tests

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -1,13 +1,13 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
 
 describe('tasks page', () => {
   it('Should render the main image', () => {
-    cy.visit('http://localhost:5173/')
-    cy.get('.main-header img').should('be.visible')
+    cy.visit('http://localhost:5173/');
+    cy.get('.main-header img').should('be.visible');
     
   });
   it('Should display the page title', () => {
-    cy.visit('http://localhost:5173/')
-    cy.get('.main-header h1').should('contain.text', 'React Tasks')
+    cy.visit('http://localhost:5173/');
+    cy.get('.main-header h1').should('contain.text', 'React Tasks');
   });
 })
